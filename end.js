8a68c01e05c2bca76917494900c5f54af70b7ce7
OpeDada <opedada@gmail.com>
2023-01-27 00:03:41 +0900

    disable save score btn, if there if user does not input a value

--- end.js
+++ end.js
@@ -1,3 +1,10 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+
+username.addEventListener("keyup", () => {
+  saveScoreBtn = !username.value;
+});
+
 saveHighScore = (e) => {
   e.preventDefault();
 };
