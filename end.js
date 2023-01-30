9af20b7872adeced0ae1001e23dbdd7c93edec1f
OpeDada <opedada@gmail.com>
2023-01-30 23:35:42 +0900

    update local storage

--- end.js
+++ end.js
@@ -21,8 +21,8 @@ saveHighScore = (e) => {
     name: username.value,
   };
   highScores.push(score);
-
   highScores.sort((a, b) => b.score - a.score);
-
   highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 };
