a6e24572f7c64d1eae38151bf779d85cacee860b
OpeDada <opedada@gmail.com>
2023-01-30 23:22:43 +0900

    create score object with score and name

--- end.js
+++ end.js
@@ -3,7 +3,7 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || []
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 finalScore.innerText = mostRecentScore;
 
@@ -13,4 +13,9 @@ username.addEventListener("keyup", () => {
 
 saveHighScore = (e) => {
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
 };
