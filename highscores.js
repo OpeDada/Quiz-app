dc49b9fd63b1826961fbf07ddb645a0429c7c4ac
OpeDada <opedada@gmail.com>
2023-02-01 23:54:49 +0900

    join array elements with an empty string

--- highscores.js
+++ highscores.js
@@ -1,6 +1,6 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScores.map(score => {
-  (`<li class="high-score">${score.name} - ${score.score} </li>`)
-})
+highScores.map((score) => {
+  return `<li class="high-score">${score.name} - ${score.score} </li>`;
+}).join("");
