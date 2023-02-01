5fdc50a8dd9ca2ba137117e44f53a424a16f775e
OpeDada <opedada@gmail.com>
2023-02-01 23:52:11 +0900

    display name and score

--- highscores.js
+++ highscores.js
@@ -2,5 +2,5 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScores.map(score => {
-  
+  (`${score.name} - ${score.score} `)
 })
