010fd68287b40704567565050f624b4b859414d5
OpeDada <opedada@gmail.com>
2023-02-01 23:56:27 +0900

    set highscores to list inner html

--- highscores.js
+++ highscores.js
@@ -1,6 +1,8 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScores.map((score) => {
-  return `<li class="high-score">${score.name} - ${score.score} </li>`;
-}).join("");
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score} </li>`;
+  })
+  .join("");
